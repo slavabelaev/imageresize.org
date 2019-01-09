d116c873ff85e2f5de44cf1a2062d19426771a1d
Slava Belaev <belaevslava@gmail.com>
2019-01-10 01:53:02 +0200

    add adaptive container

--- src/components/layouts/AppContainer/AppContainer.styles.js
+++ src/components/layouts/AppContainer/AppContainer.styles.js
@@ -2,7 +2,10 @@ const styles = theme => ({
     root: {
         maxWidth: theme.breakpoints.values.lg,
         margin: 'auto',
-        padding: theme.spacing.unit * 7.5
+        padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 2.5}px`,
+        [theme.breakpoints.up('lg')]: {
+            padding: theme.spacing.unit * 7.5,
+        }
     }
 });
 
