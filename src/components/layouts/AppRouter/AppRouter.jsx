e07747de4dc0158091648dd0f198eabd4bfab15b
Slava Belaev <belaevslava@gmail.com>
2019-01-12 19:11:14 +0200

    feat(AppRouter): add scroll to top

--- src/components/layouts/AppRouter/AppRouter.jsx
+++ src/components/layouts/AppRouter/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import MemeMakerPage from '../../pages/MemeMakerPage/MemeMakerPage';
 import ImageResizePage from '../../pages/ImageResizePage/ImageResizePage';
@@ -19,35 +19,43 @@ import PrivacyPage from '../../pages/PrivacyPage/PrivacyPage';
 import HelpPage from '../../pages/HelpPage/HelpPage';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
-const AppRouter = () => (
-    <Switch>
-        <Route path="/" exact={true} component={MemeMakerPage} />
-        <Route path="/image/resize" component={ImageResizePage} />
-        <Route path="/image/bulk-resize" component={ImageBulkResizePage} />
-        <Route path="/image/compress" component={ImageCompressPage} />
-        <Route path="/image/features" component={ImageFeaturesPage} />
+class AppRouter extends React.Component {
+    componentDidUpdate() {
+        window.scrollTo(0, 0)
+    }
+    
+    render() {
+        return (
+            <Switch>
+                <Route path="/" exact={true} component={MemeMakerPage} />
+                <Route path="/image/resize" component={ImageResizePage} />
+                <Route path="/image/bulk-resize" component={ImageBulkResizePage} />
+                <Route path="/image/compress" component={ImageCompressPage} />
+                <Route path="/image/features" component={ImageFeaturesPage} />
 
-        <Route path="/pdf/resize" component={PdfResizePage} />
-        <Route path="/pdf/compress" component={PdfCompressPage} />
-        <Route path="/pdf/merge" component={PdfMergePage} />
-        <Route path="/pdf/split" component={PdfSplitPage} />
+                <Route path="/pdf/resize" component={PdfResizePage} />
+                <Route path="/pdf/compress" component={PdfCompressPage} />
+                <Route path="/pdf/merge" component={PdfMergePage} />
+                <Route path="/pdf/split" component={PdfSplitPage} />
 
-        <Route path="/convert/jpg-to-pdf" component={ConverterPage} />
-        <Route path="/convert/jpg-to-png" component={ConverterPage} />
-        <Route path="/convert/pdf-to-jpg" component={ConverterPage} />
-        <Route path="/convert/pdf-to-png" component={ConverterPage} />
-        <Route path="/convert/png-to-jpg" component={ConverterPage} />
-        <Route path="/convert/png-to-pdf" component={ConverterPage} />
+                <Route path="/convert/jpg-to-pdf" component={ConverterPage} />
+                <Route path="/convert/jpg-to-png" component={ConverterPage} />
+                <Route path="/convert/pdf-to-jpg" component={ConverterPage} />
+                <Route path="/convert/pdf-to-png" component={ConverterPage} />
+                <Route path="/convert/png-to-jpg" component={ConverterPage} />
+                <Route path="/convert/png-to-pdf" component={ConverterPage} />
 
-        <Route path="/feature/cropping" component={FeatureCroppingPage} />
-        <Route path="/feature/editing" component={FeatureEditingPage} />
-        <Route path="/feature/compressing" component={FeatureCompressingPage} />
+                <Route path="/feature/cropping" component={FeatureCroppingPage} />
+                <Route path="/feature/editing" component={FeatureEditingPage} />
+                <Route path="/feature/compressing" component={FeatureCompressingPage} />
 
-        <Route path="/terms-of-service" component={TermsOfServicePage} />
-        <Route path="/privacy" component={PrivacyPage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-    </Switch>
-)
+                <Route path="/terms-of-service" component={TermsOfServicePage} />
+                <Route path="/privacy" component={PrivacyPage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        )
+    }
+}
 
-export default AppRouter;
+export default withRouter(AppRouter);
