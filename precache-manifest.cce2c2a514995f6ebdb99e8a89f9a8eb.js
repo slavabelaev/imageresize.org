b89e951f803493d0a6900f42043ac504a9f674f3
Slava Belaev <slav@belaev.pro>
2021-11-01 23:38:45 +0300

    Updates

---
+++ precache-manifest.cce2c2a514995f6ebdb99e8a89f9a8eb.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2f2474488c0822e4186",
+    "url": "/imageresize.org/static/css/main.679388d0.chunk.css"
+  },
+  {
+    "revision": "c2f2474488c0822e4186",
+    "url": "/imageresize.org/static/js/main.c2f24744.chunk.js"
+  },
+  {
+    "revision": "f27582c498f93d1dc9f0",
+    "url": "/imageresize.org/static/js/1.f27582c4.chunk.js"
+  },
+  {
+    "revision": "6935970e883e0dfc1ffb",
+    "url": "/imageresize.org/static/js/runtime~main.6935970e.js"
+  },
+  {
+    "revision": "a6e672ea8bd70f87c78f18d40b36e3cc",
+    "url": "/imageresize.org/static/media/AppLogo.a6e672ea.svg"
+  },
+  {
+    "revision": "5480b032d6fad35ad24f709dbaeecf34",
+    "url": "/imageresize.org/static/media/converter.5480b032.svg"
+  },
+  {
+    "revision": "97c6ff6b8da5bc81b318866839d7d6a9",
+    "url": "/imageresize.org/static/media/envelope.97c6ff6b.svg"
+  },
+  {
+    "revision": "6719b99515e13f3044af5164df68bd7d",
+    "url": "/imageresize.org/static/media/facebook.6719b995.svg"
+  },
+  {
+    "revision": "befd3df96fd4dcf867ea49f6682523e8",
+    "url": "/imageresize.org/static/media/facebook_dark.befd3df9.svg"
+  },
+  {
+    "revision": "9acc7a39410a4d6024bcf95996aca9a6",
+    "url": "/imageresize.org/static/media/features.9acc7a39.svg"
+  },
+  {
+    "revision": "664a60a33e2a6709f2fa98de25a454b2",
+    "url": "/imageresize.org/static/media/file.664a60a3.svg"
+  },
+  {
+    "revision": "1675d4beb7dd6674538fb4be186bf930",
+    "url": "/imageresize.org/static/media/google-plus.1675d4be.svg"
+  },
+  {
+    "revision": "62779c422261e7ca68ed6de2a7ba0228",
+    "url": "/imageresize.org/static/media/google-plus_dark.62779c42.svg"
+  },
+  {
+    "revision": "6c9a1d79115b36799cbaba264b1a9e4f",
+    "url": "/imageresize.org/static/media/image.6c9a1d79.svg"
+  },
+  {
+    "revision": "9bf281f1bd33dad9ca32b4c0741dbdc0",
+    "url": "/imageresize.org/static/media/pdf.9bf281f1.svg"
+  },
+  {
+    "revision": "f46ccf22545a5a09cb274108679c005e",
+    "url": "/imageresize.org/static/media/pinterest.f46ccf22.svg"
+  },
+  {
+    "revision": "ad333fe7b6fbbe673013b51a78819256",
+    "url": "/imageresize.org/static/media/reddit.ad333fe7.svg"
+  },
+  {
+    "revision": "7a4d9f0fe157437d3258bbc3b785066d",
+    "url": "/imageresize.org/static/media/twitter.7a4d9f0f.svg"
+  },
+  {
+    "revision": "5b79f60c35eabb5e743159e3152b6310",
+    "url": "/imageresize.org/static/media/twitter_dark.5b79f60c.svg"
+  },
+  {
+    "revision": "931870f19852efd1692ffec6abc295e5",
+    "url": "/imageresize.org/static/media/PdfMergeTool.cover.931870f1.svg"
+  },
+  {
+    "revision": "57662d5b00f1fee4311f621ee73f2549",
+    "url": "/imageresize.org/static/media/cropping-1.57662d5b.jpg"
+  },
+  {
+    "revision": "eac3661699b453d0cc63f66f1e22f9bf",
+    "url": "/imageresize.org/static/media/cropping-2.eac36616.jpg"
+  },
+  {
+    "revision": "96297a31e4b66d91944bc3fc9018ca4e",
+    "url": "/imageresize.org/static/media/editing-1.96297a31.jpg"
+  },
+  {
+    "revision": "1441f53590044bae12276005aef015fc",
+    "url": "/imageresize.org/static/media/editing-2.1441f535.jpg"
+  },
+  {
+    "revision": "8ba3a929c23a5cbb80f75f8b6745dcb5",
+    "url": "/imageresize.org/static/media/editing-3.8ba3a929.jpg"
+  },
+  {
+    "revision": "d5952c31a7c55568a51c41675fffeff0",
+    "url": "/imageresize.org/static/media/editing-4.d5952c31.jpg"
+  },
+  {
+    "revision": "765607dc60d5e643d318b8014af7cd9f",
+    "url": "/imageresize.org/static/media/compressing-1.765607dc.jpg"
+  },
+  {
+    "revision": "63f13d12b0ff7105bbde2a98842e1be1",
+    "url": "/imageresize.org/static/media/compressing-2.63f13d12.jpg"
+  },
+  {
+    "revision": "7bf8a0ed752187ea4d16d9fac8bb9040",
+    "url": "/imageresize.org/static/media/GothamPro-BoldItalic.7bf8a0ed.woff"
+  },
+  {
+    "revision": "4088b8129bd1c4b3a2a352e53a26f7ba",
+    "url": "/imageresize.org/static/media/GothamPro-BoldItalic.4088b812.eot"
+  },
+  {
+    "revision": "0b468847d196e2c13527704d24d266e7",
+    "url": "/imageresize.org/static/media/GothamPro-BoldItalic.0b468847.ttf"
+  },
+  {
+    "revision": "71f3484873a700b056ba019138925996",
+    "url": "/imageresize.org/static/media/GothamPro-Italic.71f34848.woff"
+  },
+  {
+    "revision": "37ed223f1f7d850af5fb069d2e42b2c1",
+    "url": "/imageresize.org/static/media/GothamPro-Italic.37ed223f.eot"
+  },
+  {
+    "revision": "628b3e58e45479eabd863d42c083f1d3",
+    "url": "/imageresize.org/static/media/GothamPro-Italic.628b3e58.ttf"
+  },
+  {
+    "revision": "6fe466da15644ccc370d3b2ce3a19f6f",
+    "url": "/imageresize.org/static/media/GothamPro-Medium.6fe466da.eot"
+  },
+  {
+    "revision": "b86e995a10856a8b8e222fe8ff00a74f",
+    "url": "/imageresize.org/static/media/GothamPro-Medium.b86e995a.woff"
+  },
+  {
+    "revision": "c161369173f822acd66d2ff8eea64c52",
+    "url": "/imageresize.org/static/media/GothamPro-Medium.c1613691.ttf"
+  },
+  {
+    "revision": "e79aec4e580a0fd95f93e7ba53a6b92a",
+    "url": "/imageresize.org/static/media/GothamPro-BlackItalic.e79aec4e.woff"
+  },
+  {
+    "revision": "ee9b8c537d217fef51d4bca8cf436d7d",
+    "url": "/imageresize.org/static/media/GothamPro-Bold.ee9b8c53.eot"
+  },
+  {
+    "revision": "148d45efc780297e8b005b44b735068d",
+    "url": "/imageresize.org/static/media/GothamPro-BlackItalic.148d45ef.eot"
+  },
+  {
+    "revision": "c15ee62b232cedc240947b6d814fb750",
+    "url": "/imageresize.org/static/media/GothamPro-Bold.c15ee62b.ttf"
+  },
+  {
+    "revision": "a3d7d652af07c3610c453b4a51c788c7",
+    "url": "/imageresize.org/static/media/GothamPro-Bold.a3d7d652.woff"
+  },
+  {
+    "revision": "99a2e7292d9fd291ccad32cef31cd7a2",
+    "url": "/imageresize.org/static/media/GothamPro-LightItalic.99a2e729.woff"
+  },
+  {
+    "revision": "706777597539538a8b9db05c1a954b4a",
+    "url": "/imageresize.org/static/media/GothamPro-BlackItalic.70677759.ttf"
+  },
+  {
+    "revision": "f083fa2d4cb6eef10b43f3db3a4e488c",
+    "url": "/imageresize.org/static/media/GothamPro-LightItalic.f083fa2d.eot"
+  },
+  {
+    "revision": "7fd3861de823f75a6923000a948c494a",
+    "url": "/imageresize.org/static/media/GothamPro-Light.7fd3861d.eot"
+  },
+  {
+    "revision": "b5a02ab95cf7db396b81967ec96cfc6a",
+    "url": "/imageresize.org/static/media/GothamPro-LightItalic.b5a02ab9.ttf"
+  },
+  {
+    "revision": "677c3a32938f905b22eb14afd1d5cff4",
+    "url": "/imageresize.org/static/media/GothamPro-Light.677c3a32.woff"
+  },
+  {
+    "revision": "abd5115e2ddae490f9cb393fb8cdfc5a",
+    "url": "/imageresize.org/static/media/GothamPro-Light.abd5115e.ttf"
+  },
+  {
+    "revision": "3309bc970fb9a0c54595de5973a365e9",
+    "url": "/imageresize.org/static/media/GothamPro-Black.3309bc97.eot"
+  },
+  {
+    "revision": "d563ad87622a3dfeea9ff9ee22678caa",
+    "url": "/imageresize.org/static/media/GothamPro-Black.d563ad87.woff"
+  },
+  {
+    "revision": "c31a8e097cb6c2901857d8c686f0063d",
+    "url": "/imageresize.org/static/media/GothamPro.c31a8e09.eot"
+  },
+  {
+    "revision": "efb0c02a5dbe65a7115b477e74c7a661",
+    "url": "/imageresize.org/static/media/GothamPro-Black.efb0c02a.ttf"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/imageresize.org/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "b41fe76688645088f332806c78935454",
+    "url": "/imageresize.org/static/media/GothamPro-MediumItalic.b41fe766.eot"
+  },
+  {
+    "revision": "3dd45641b4fb03c2c3fa2e96a513de9b",
+    "url": "/imageresize.org/static/media/GothamPro-MediumItalic.3dd45641.woff"
+  },
+  {
+    "revision": "61765145d603e0f13473257e6ab6e0b9",
+    "url": "/imageresize.org/static/media/GothamPro-MediumItalic.61765145.ttf"
+  },
+  {
+    "revision": "3cb46b372ab77d032576b9f70c83d1ff",
+    "url": "/imageresize.org/static/media/GothamPro.3cb46b37.woff"
+  },
+  {
+    "revision": "53afa4fa06e3d67376d6b91f3e4b6b5a",
+    "url": "/imageresize.org/static/media/background-left.53afa4fa.jpg"
+  },
+  {
+    "revision": "3dc9c34611c060c92d47e9e49f6b2083",
+    "url": "/imageresize.org/static/media/background-right.3dc9c346.jpg"
+  },
+  {
+    "revision": "f72c71c8526451d74e1034e74e0b7977",
+    "url": "/imageresize.org/index.html"
+  }
+];
